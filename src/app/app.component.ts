9c1fa89661f796a8fe7b28eba701b8cf2a43c83f
unknown <margarita.alvarez3105@gmail.com>
2023-06-12 12:45:26 -0600

    final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados-app';
+  dadoIzquierdo ='../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice4.png';
+  numero1: number;
+  numero2: number;
+
+  tirarDados(): void {
+    this.numero1= Math.round(Math.random() *5)+1;
+    this.numero2 =Math.round(Math.random() *5)+1;
+    this.dadoIzquierdo ='../assets/img/dice' + this.numero1 + 'png';
+    this.dadoDerecho ='../assets/img/dice' + this.numero2 + 'png';
+  }
 }
